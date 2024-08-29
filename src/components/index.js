c21c613c99c0450670d99e2bee073b8a9e7ba844
Shashi Bhushan Singh <shashibhushan3105@gmail.com>
2024-08-29 15:28:27 +0530

    Pages

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,23 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-
+import RTE from "./RTE";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
 
 }
